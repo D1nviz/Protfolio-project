3b2de5c40517f5b42fbe4c1700d40f5aefb6c48e
Dinviz <eugene.hetsyanyn@gmail.com>
2023-06-10 19:39:51 +0300

    bug fix

--- src/components/AboutAuthor/index.tsx
+++ src/components/AboutAuthor/index.tsx
@@ -6,22 +6,21 @@ import {
   AboutDescContainer,
   AboutTitle,
 } from "./styles";
-interface IAboutAuthor {}
 
-const AboutAuthor: FC<IAboutAuthor> = () => {
+const AboutAuthor: FC = () => {
   return (
     <AboutContainer>
       <AboutDescContainer>
         <AboutTitle>Biography</AboutTitle>
         <AboutDesc>
-          Hi, I'm Dinviz, a web developer and UI/UX designer with a passion
+          Hi, I&apos;m Dinviz, a web developer and UI/UX designer with a passion
           for creating beautiful, functional, and user-centered digital
           experiences.
           <br /> With 4 years of experience in the field. I am always looking
-          for new and innovative ways to bring my clients' visions to life.
+          for new and innovative ways to bring my clients&apos; visions to life.
           <br /> <br /> I believe that design is about more than just making
-          things look pretty – it's about solving problems and creating
-          intuitive, enjoyable experiences for users. <br /> <br /> Whether I'm
+          things look pretty – it&apos;s about solving problems and creating
+          intuitive, enjoyable experiences for users. <br /> <br /> Whether I&apos;m
           working on a website, mobile app, or other digital product, I bring my
           commitment to design excellence and user-centered thinking to every
           project I work on. I look forward to the opportunity to bring my
